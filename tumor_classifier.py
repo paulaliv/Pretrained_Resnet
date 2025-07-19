7546d4d675297bae20abc2662c495fd8d28e6f01
paulaliv <paula.alfken@gmail.com>
2025-07-19 14:14:06 +0200

    hallo

--- tumor_classifier.py
+++ tumor_classifier.py
@@ -234,7 +234,8 @@ def train_one_fold(model, preprocessed_dir, plot_dir, fold_paths, optimizer, sch
     loss_function = FocalLoss(
         to_onehot_y= True,
         use_softmax=True,
-        gamma=2.0
+        gamma=2.0,
+        class_weight=class_weights
     )
 
     scaler = GradScaler()
